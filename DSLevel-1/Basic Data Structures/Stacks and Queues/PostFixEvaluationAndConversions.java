643371197eec16a1a978a8ae48328d77d0e9220f
Akanksha137 <93490784+Akanksha137@users.noreply.github.com>
2021-11-09 00:12:36 +0530

    12:122

--- DSLevel-1/Basic Data Structures/Stacks and Queues/PostFixEvaluationAndConversions.java
+++ DSLevel-1/Basic Data Structures/Stacks and Queues/PostFixEvaluationAndConversions.java
@@ -9,5 +9,34 @@ public static void main(String[] args) throws Exception {
     String exp = br.readLine();
 
     // code
+    Stack<Integer> operand = new Stack<>();
+   // Stack<Character> operator = new Stack<>();
+    for(int  i=0;i<exp.length();i++){
+        char ch = exp.charAt(i);
+        if(Character.isDigit(ch)){
+            operand.push(ch-'0');
+        }
+        else{
+            int v2 = operand.pop();
+            int v1 = operand.pop();
+            int k = operation(v1,v2,ch);
+            operand.push(k);
+        }
+    }
+    System.out.println(operand.peek());
+ }
+ public static int operation(int v1 , int v2 ,char ch){
+     if(ch == '+'){
+         return v1+v2;
+     }
+     else if(ch =='-'){
+         return v1-v2;
+     }
+     else if(ch == '*'){
+         return v1*v2;
+     }
+     else{
+         return v1/v2;
+     }
  }
 }
