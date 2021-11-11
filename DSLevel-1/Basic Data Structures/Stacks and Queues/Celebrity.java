80ed981eac143ba681f5c31fdbf71f103806c520
Akanksha137 <93490784+Akanksha137@users.noreply.github.com>
2021-11-11 10:50:54 +0530

    Celebrity

--- DSLevel-1/Basic Data Structures/Stacks and Queues/Celebrity.java
+++ DSLevel-1/Basic Data Structures/Stacks and Queues/Celebrity.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class Calebrity {
+public class Celebrity {
 
     public static void main(String[] args) throws Exception {
         // write your code here
@@ -23,6 +23,34 @@ public class Calebrity {
     public static void findCelebrity(int[][] arr) {
         // if a celebrity is there print it's index (not position), if there is not then
         // print "none"
+
+        Stack<Integer> st = new Stack<>();
+        for(int i=0;i<arr.length;i++){
+            st.push(i);
+        }
+
+        while(st.size()>=2){
+           int i=st.pop();
+           int j = st.pop();
+           if(arr[i][j]==1){
+               // i --> not a celebrity
+               st.push(j);
+           } 
+           else{
+               // j --> not a celebrity
+               st.push(i);
+           }
+        }
+        int pot = st.pop();
+        for(int i=0;i<arr.length ; i++){
+            if(i != pot){
+                if(arr[i][pot]== 0  || arr[pot][i]==1){
+                    System.out.println("none");
+                    return;
+                }
+            }
+        }
+        System.out.println(pot);
     }
 
 }
---
+++ DSLevel-1/Basic Data Structures/Stacks and Queues/MergeOverlappingInterval.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class MergeOverlappingInterval {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[][] arr = new int[n][2];
+
+        for (int j = 0; j < n; j++) {
+            String line = br.readLine();
+            arr[j][0] = Integer.parseInt(line.split(" ")[0]);
+            arr[j][1] = Integer.parseInt(line.split(" ")[1]);
+        }
+
+        mergeOverlappingIntervals(arr);
+    }
+
+    public static void mergeOverlappingIntervals(int[][] arr) {
+        // merge overlapping intervals and print in increasing order of start time
+    }
+
+}
