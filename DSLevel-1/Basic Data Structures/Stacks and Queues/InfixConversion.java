1ac7a7bdb1fad6a24c2454b86641acf319d74af3
Akanksha137 <93490784+Akanksha137@users.noreply.github.com>
2021-11-14 20:50:34 +0530

    infix

--- DSLevel-1/Basic Data Structures/Stacks and Queues/InfixConversion.java
+++ DSLevel-1/Basic Data Structures/Stacks and Queues/InfixConversion.java
@@ -2,53 +2,68 @@ import java.io.*;
 import java.util.*;
 
 public class InfixConversion{
-  
-
-public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     String exp = br.readLine();
 
-    // code
-    
     Stack<String>postfix = new Stack<>();
+    Stack<String>prefix = new Stack<>();
     Stack<Character>operators = new Stack<>();
     for(int i=0;i<exp.length();i++){
         char ch = exp.charAt(i);
         if(ch == ')'){
-            // while(operators.size()>0 && operators.peek()!='('){
-            //     String v2 = postfix.pop();
-            //     String v1 = postfix.pop();
-            //     char c = operators.pop();
-            //     String str = v2 + v1 +ch;
-            //     postfix.push(str);
-            // }
-            //postfix.pop();
+            while(operators.size()>0 && operators.peek()!='('){
+                String pv2 = postfix.pop();
+                String pv1 = postfix.pop();
+                char c = operators.pop();
+                String str =  pv1 + pv2 + c;
+                postfix.push(str);
+                String prev2 = prefix.pop();
+                String prev1 = prefix.pop();
+                String prestr = c + prev1 + prev2 ;
+                prefix.push(prestr);
+            }
+            operators.pop();
         }
         else if(ch =='('){
-           //operators.push(ch); 
+          operators.push(ch);
         }
         
         else if(ch == '+' || ch == '-' || ch== '*' || ch == '/'){
-            // while(postfix.size()>0 && operators.size()>0 && preferences(ch)<= preferences(operators.peek())){
-            //     String pv2 = postfix.pop();
-            //     String pv1 = postfix.pop();
-            //     String str =  pv1 + pv2 + ch;
-            //     postfix.push(str);
-            // }
-
-            while(postfix.size()>1){
+            while(operators.size()>0 && preferences(ch)<=preferences(operators.peek()) &&  operators.peek()!='('){
                 String pv2 = postfix.pop();
                 String pv1 = postfix.pop();
-                String str =  pv1 + pv2 + ch;
+                char c= operators.pop();
+                String str =  pv1 + pv2 + c;
                 postfix.push(str);
+                String prev2 = prefix.pop();
+                String prev1 = prefix.pop();
+                String prestr = c + prev1 + prev2 ;
+                prefix.push(prestr);
             }
+            operators.push(ch);
         }
         else{
             postfix.push(ch+"");
+            prefix.push(ch+"");
         }
     }
-    System.out.println(postfix);
+     while(operators.size()>0){
+                String pv2 = postfix.pop();
+                String pv1 = postfix.pop();
+                char c = operators.pop();
+                String str =  pv1 + pv2 + c;
+                postfix.push(str);
+                 String prev2 = prefix.pop();
+                String prev1 = prefix.pop();
+                String prestr = c + prev1 + prev2 ;
+                prefix.push(prestr);
+            }
+    
+    System.out.println(postfix.peek());
+     System.out.println(prefix.peek());
  }
+
  public static int preferences(char ch){
      if(ch == '+'){
          return 1;
