ce6384c2a0075512738915d9ffd1fd803794cbef
Akanksha137 <93490784+Akanksha137@users.noreply.github.com>
2021-12-11 18:30:43 +0530

    symmetrer

--- DSLevel-1/Basic Data Structures/Generic Tree/IsGenericTreeSymmetric.java
+++ DSLevel-1/Basic Data Structures/Generic Tree/IsGenericTreeSymmetric.java
@@ -79,25 +79,23 @@ public class IsGenericTreeSymmetric {
     return h;
   }
 
- public static boolean IsSymmetric(Node node) {
-    for(int i=0;i<node.children.size()/2;i++){
-        Node c1 = node.children.get(i);
-        Node c2 = node.children.get(node.children.size()-1-i);
-        if(c1.children.size() != c2.children.size()){
-            return false;
-        }
-    }
-    if(node.children.size()%2 >0){
-         Node child = node.children.get(node.children.size()/2);
-         boolean res = IsSymmetric(child);
-          if(res == false){
-            return false;
-        }
-    }
-   
-    return true;
-  }
+ private static boolean areMirror(Node n1, Node n2) {
+  if (n1.children.size() != n2.children.size()) {
+    return false;
 
+  }
+  for (int i = 0; i < n1.children.size(); i++) {
+    Node c1 = n1.children.get(i);
+    Node c2 = n2.children.get(n2.children.size() - 1 - i);
+    if (areMirror(c1, c2) == false) {
+      return false;
+    }
+  }
+  return true;
+}
+public static boolean IsSymmetric(Node node) {
+  return areMirror(node, node);
+}
   public static void main(String[] args) throws Exception {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     int n = Integer.parseInt(br.readLine());
