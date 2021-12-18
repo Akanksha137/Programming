1c3791fa2ec3080237612ee315b32b1c1e2d6b27
Akanksha137 <93490784+Akanksha137@users.noreply.github.com>
2021-12-18 12:17:45 +0530

    maximu subtree

---
+++ DSLevel-1/Basic Data Structures/Generic Tree/NodeMaximumSubtree.java
@@ -0,0 +1,79 @@
+import java.io.*;
+import java.util.*;
+
+public class NodeMaximumSubtree {
+  private static class Node {
+    int data;
+    ArrayList<Node> children = new ArrayList<>();
+  }
+
+  public static void display(Node node) {
+    String str = node.data + " -> ";
+    for (Node child : node.children) {
+      str += child.data + ", ";
+    }
+    str += ".";
+    System.out.println(str);
+
+    for (Node child : node.children) {
+      display(child);
+    }
+  }
+
+  public static Node construct(int[] arr) {
+    Node root = null;
+
+    Stack<Node> st = new Stack<>();
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] == -1) {
+        st.pop();
+      } else {
+        Node t = new Node();
+        t.data = arr[i];
+
+        if (st.size() > 0) {
+          st.peek().children.add(t);
+        } else {
+          root = t;
+        }
+
+        st.push(t);
+      }
+    }
+
+    return root;
+  }
+    
+    static int nodeData ;
+    static int max;
+    
+    private static int maxValue(Node node){
+        int val = 0;
+        
+      for(Node child :node.children){
+         val += maxValue(child);  
+      }
+      val += node.data;
+       if(val > max){
+           max = val;
+           nodeData = node.data;
+       }
+      return val;
+    }
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    int[] arr = new int[n];
+    String[] values = br.readLine().split(" ");
+    for (int i = 0; i < n; i++) {
+      arr[i] = Integer.parseInt(values[i]);
+    }
+
+    Node root = construct(arr);
+    nodeData = 0;
+    max = 0;
+    maxValue(root);
+    System.out.println(nodeData + "@"+max);
+  }
+
+}
