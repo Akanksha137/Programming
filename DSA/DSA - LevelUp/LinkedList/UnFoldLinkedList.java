2d391d7d12acea7f6fd04bee8891095fae749e30
Akanksha137 <93490784+Akanksha137@users.noreply.github.com>
2022-06-27 11:01:12 +0530

    unfold.java

--- DSA/DSA - LevelUp/LinkedList/UnFoldLinkedList.java
+++ DSA/DSA - LevelUp/LinkedList/UnFoldLinkedList.java
@@ -10,8 +10,64 @@ class UnFoldLinkedList {
         }
     }
 
+    
     public static void unfold(ListNode head) {
+    
+    // ListNode even = new ListNode(-1);
+    // ListNode odd = new ListNode(-1);
+    // ListNode evenStart = even;
+    // ListNode oddStart = odd;
+    // boolean isEven = true;
+    // while(head!=null){
+    //     if(isEven == true){
+    //         even.next = head;
+    //         even = even.next;
+    //         isEven = false;
+    //     }
+    //     else{
+    //         odd.next = head;
+    //         odd = odd.next;
+    //         isEven = true;
+    //     }
+    //     head = head.next;
+    // }
+
+    // odd.next = null;
+    // even.next = null;
+
+    // ListNode reverseLL =reverse(oddStart.next);
+    // head = evenStart.next;
+    // even.next = reverseLL;
+
+
+
+    // Optimisation
+
+    //first
+    ListNode fh = head;
+    ListNode fp = fh;
+    
+    //second
+    ListNode sh = head.next;
+    ListNode sp = sh;
+    
+    while(sp !=null && sp.next!=null){
+        //backup
+        ListNode f = sp.next;
+
+        //links
+        fp.next = f;
+        sp.next = f.next;
         
+        //move
+        fp = fp.next;
+        sp = sp.next;
+    }
+
+    fp.next = null;
+    sh = reverse(sh);
+    fp.next = sh;
+
     }
 
     static void printList(ListNode node) {
@@ -19,6 +75,19 @@ class UnFoldLinkedList {
             System.out.print(node.val + " ");
             node = node.next;
         }
+    }
+
+    public static ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        ListNode cur = head;
+        while(cur.next!=null){
+            ListNode next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        cur.next = prev;
+        return cur;
     }
 
     public static void main(String[] args) {
